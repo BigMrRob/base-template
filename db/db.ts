0a7e63827969e153b819c990d289ac99f9f02006
Robert Farris <rfarris@inrhythm.com>
2025-02-23 10:56:28 -0600

    First commit

--- db/db.ts
+++ db/db.ts
@@ -9,13 +9,23 @@ import { config } from "dotenv"
 import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
 
-config({ path: ".env.local" })
+config({ path: ".env" })
 
 const schema = {
   profiles: profilesTable,
   todos: todosTable
 }
 
-const client = postgres(process.env.DATABASE_URL!)
+const client = postgres(process.env.DATABASE_URL!, {
+  ssl: {
+    rejectUnauthorized: false
+  },
+  max: 1,
+  idle_timeout: 20,
+  connect_timeout: 30,
+  connection: {
+    options: "--client_encoding=UTF8"
+  }
+})
 
 export const db = drizzle(client, { schema })
---
+++ db/migrations/0001_next_reptil.sql
@@ -0,0 +1,2 @@
+ALTER TYPE "membership" ADD VALUE 'basic';--> statement-breakpoint
+ALTER TYPE "membership" ADD VALUE 'standard';
